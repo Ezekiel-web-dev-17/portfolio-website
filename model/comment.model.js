31e212996c5f3e583faae890e61e4162d0bcbfd9
Tabuke Ezekiel <tabukeezekiel9@gmail.com>
2025-08-28 00:04:36 +0100

    Added .env to gitIgnore

--- model/comment.model.js
+++ model/comment.model.js
@@ -13,7 +13,6 @@ const commentSchema = new Schema(
     email: {
       type: String,
       required: [true, "Your email is required"],
-      unique: true,
       trim: true,
       lowercase: true,
       match: [/\S+@\S+\.\S+/, "Please fill a valid email address"],
